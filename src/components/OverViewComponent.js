974b41e4c84bbf360f5317a7a37394e53cb161ee
Samaheh Loghmani <>
2021-12-03 10:08:15 -0500

    created overniew component

---
+++ src/components/OverViewComponent.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import TransActionForm from "./TransActionForm";
+
+const OverViewComponent = ({ income, expense, addTransaction }) => {
+    const [isShow, setIsShow] = useState(false);
+    return (
+        <>
+            <div className="topSection">
+                <p> Balance : {income - expense}</p>
+                <button
+                    onClick={() => setIsShow((prevState) => !prevState)}
+                    className={`btn ${isShow && "cancel"}`}
+                >
+                    {isShow ? "Cancel" : "Add"}
+                </button>
+            </div>
+            {isShow && (
+                <TransActionForm
+                    addTransaction={addTransaction}
+                    setIsShow={setIsShow}
+                />
+            )}
+            <div className="resultSection">
+                <div className="expenseBox">
+                    Expense<span style={{ color: "red" }}> {expense} $</span>
+                </div>
+                <div className="expenseBox">
+                    income <span>{income} $</span>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default OverViewComponent;
